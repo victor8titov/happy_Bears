783083e677019f0d2eba7408a2e84cd1f49fad40
w8lt8r <victor8titov@ya.ru>
2019-11-29 18:01:17 +0300

    init and ready config webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: ["/node_modules/"],
+            use: [
+              {
+                loader: "babel-loader",
+                options: {
+                  presets: ["env"],
+                },
+              },
+            ],
+          },
+          {
+			test: /\.scss$/,
+			use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { sourceMap: true }
+					}, {
+						loader: 'postcss-loader',
+						options: { sourceMap: true, config: { path: 'postcss.config.js' } }
+					}, {
+						loader: 'sass-loader',
+					options: { sourceMap: true }
+					}
+				]
+			}
+        ],
+	   },
+	   plugins: [
+		new MiniCssExtractPlugin({
+		  filename: "main.css",
+		})
+	  ],
+       devServer: {
+        contentBase: './dist',
+        open: true
+      },
+}

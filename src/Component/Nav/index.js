ec3135428097f7b4ceaf24320574443635cdddc1
w8lt8r <victor8titov@ya.ru>
2019-12-02 14:08:31 +0300

    commit

--- src/Component/Nav/index.js
+++ src/Component/Nav/index.js
@@ -6,32 +6,70 @@ class Nav extends Component {
         super(props);
         this.onChangeCheckbox = this.onChangeCheckbox.bind(this);
         this.onChangeSelect = this.onChangeSelect.bind(this);
-        this.flagReserve = undefined;
+        this.flagReserve = false;
         this.flagStatus = undefined;
     }
     onChangeSelect(e) {
         this.flagStatus = e.target.value;
-        let filter=e.target.value;
         let listElement = [...document.querySelectorAll(this.props.filterSelect)];
-        listElement = listElement.filter((elm)=>this.props.isChecked(elm, this.flagReserve))
+        
+        if (this.flagReserve !== undefined) {
+            listElement = listElement.filter((elm)=>{
+                if ( !this.props.isChecked(elm, this.flagReserve) ) {
+                    //this.Hidden(elm);
+                    return false;
+                } else {
+                    //this.Show(elm);
+                    return true;
+                }
+            })
+        }
 
-        listElement.forEach((elm)=> this.props.isSelect(elm,filter) ? this.Show(elm) : this.Hidden(elm) )
+        listElement.forEach((elm)=> this.props.isSelect(elm,this.flagStatus) ? this.Show(elm) : this.Hidden(elm) );
+        
     }
     onChangeCheckbox(e) {
         this.flagReserve = e.target.checked;
-
-        let listElement = document.querySelectorAll(this.props.filterChecked);
+        let listElement = [...document.querySelectorAll(this.props.filterChecked)];
+        
+        if (this.flagStatus !== undefined) {
+            listElement = listElement.filter((elm)=>{
+                if ( !this.props.isSelect(elm, this.flagStatus) ) {
+                    //this.Hidden(elm);
+                    return false;
+                } else {
+                    //this.Show(elm);
+                    return true;
+                }
+            })
+        }
         listElement.forEach((elm)=> this.props.isChecked(elm,this.flagReserve) ? this.Show(elm) : this.Hidden(elm) )
+        
+    }
+    sorting() {
+
+        let listElement = [...document.querySelectorAll(this.props.filterSelect)];
+        listElement = listElement.filter((elm)=>{
+            if ( !this.props.isChecked(elm, this.flagReserve) ) {
+                this.Hidden(elm);
+                return false;
+            } else {
+                this.Show(elm);
+                return true;
+            }
+        })
+
+        if (this.flagStatus === undefined) return;
+        listElement.forEach((elm)=> this.props.isSelect(elm,filter) ? this.Show(elm) : this.Hidden(elm) )
     }
     Show(elm){
-        elm.classList.remove('absolute');
-        //elm.classList.add('static');
+        elm.classList.remove('absolute')
         elm.classList.remove('hidden'); 
     }
     Hidden(elm) {
         elm.classList.add('hidden');
         //elm.classList.remove('static');
-        setTimeout(()=>elm.classList.add('absolute'),400); 
+        setTimeout(()=>elm.classList.add('absolute'),4400); 
     }
     render() {
         return (
